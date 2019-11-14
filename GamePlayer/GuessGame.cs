ddaf0f41117abdfe0bbda1a30f497cb10a077062
andy110414 <aeeoprsd110414@gmail.com>
2019-11-14 19:47:00 +0800

    測試

---
+++ GamePlayer/GuessGame.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamePlayer
+{
+    public class GuessGame : IGame
+    {
+        int max = 0;
+        public void Play()
+        {
+            Console.WriteLine($"你輸入的最大範圍是{max}");
+        }
+
+        public void Setup()
+        {
+            Console.WriteLine("請輸入範圍:");
+            string input = Console.ReadLine();
+            max = int.Parse(input);
+        }
+    }
+}
